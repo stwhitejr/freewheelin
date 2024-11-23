333f5b663595817f6c30707e52a3c17f6f51ab8c
Steve White <stwhite@wayfair.com>
2024-11-23 14:23:08 -0700

    firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "test"));
+      const documents = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setData(documents);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data)
+
+  return <div>hello world</div>
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6etn3ynRVa-bRD4KFBhd6BjuVbDDHaG0",
+  authDomain: "freewheelin-5ff80.firebaseapp.com",
+  projectId: "freewheelin-5ff80",
+  storageBucket: "freewheelin-5ff80.firebasestorage.app",
+  messagingSenderId: "511685866642",
+  appId: "1:511685866642:web:c7a47c9b28e5ed9977a24b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
