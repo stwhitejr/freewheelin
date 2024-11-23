4c2bf642bd5ba28c161613996bb063a443a5950d
Steve White <stwhite@wayfair.com>
2024-11-23 14:55:19 -0700

    storage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { db } from "./firebase";
 import { collection, getDocs } from "firebase/firestore";
 import useGetPhotos from "./useGetPhotos";
+import useUploadPhoto from "./useUploadPhoto";
 
 const App = () => {
-  const file = useGetPhotos();
+  const [file, setFile] = useState(null)
+  const photo = useGetPhotos();
+  const {upload, progress, fileUrl} =   useUploadPhoto();
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -17,10 +20,21 @@ const App = () => {
     fetchData();
   }, []);
 
-  console.log(data)
-  console.log(file)
 
-  return <div>hello world</div>
+
+  console.log(data)
+  console.log(photo)
+
+  return <div>
+    <div>
+      <div>hello world</div>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+      {file && (<button onClick={() => upload(file)}>Upload</button>)}
+
+      <p>Progress: {progress}%</p>
+      {fileUrl && <img src={fileUrl} alt="Uploaded Image" />}
+    </div>
+  </div>
 }
 
 export default App;
---
+++ src/useUploadPhoto.js
@@ -0,0 +1,38 @@
+import { useCallback, useState } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+
+const useUploadPhoto = () => {
+  const [progress, setProgress] = useState(0);
+  const [fileUrl, setDownloadURL] = useState(null);
+
+  return {
+    upload: useCallback((file) => {
+      const storage = getStorage();
+      const storageRef = ref(storage, `images/${file.name}`);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progressPercent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progressPercent);
+        },
+        (error) => {
+          console.error('Error uploading file:', error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            setDownloadURL(url);
+          });
+        }
+      );
+    }, []),
+    fileUrl,
+    progress
+  }
+}
+
+export default useUploadPhoto;
